6183403e3192f2aa9653ae8422a6bbaeb92b7178
guv <jurapro@yandex.ru>
2023-10-23 10:39:50 +0700

    add User model

--- demo/models.py
+++ demo/models.py
@@ -1,4 +1,25 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+
 
 # Create your models here.
 
+# Create your models here.
+class User(AbstractUser):
+    name = models.CharField(max_length=254, verbose_name='Имя', blank=False)
+    surname = models.CharField(max_length=254, verbose_name='Фамилия', blank=False)
+    patronymic = models.CharField(max_length=254, verbose_name='Отчество', blank=True)
+    username = models.CharField(max_length=254, verbose_name='Логин', unique=True, blank=False)
+    email = models.CharField(max_length=254, verbose_name='Почта', unique=True, blank=False)
+    password = models.CharField(max_length=254, verbose_name='Пароль', blank=False)
+    role = models.CharField(max_length=254, verbose_name='Роль',
+                            choices=(('admin', 'Администратор'), ('user', 'Пользователь')), default='user')
+
+    USERNAME_FIELD = 'username'
+
+    def full_name(self):
+        # return self.name + ' ' + self.surname + ' ' + self.patronymic
+        return ' '.join([self.name, self.surname, self.patronymic])
+
+    def __str__(self):
+        return self.full_name()
